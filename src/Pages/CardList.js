905656d4e3ecfac67c3760e3972e5b6ebb7000cb
Nerd <americo.f.b.f@gmail.com>
2024-10-05 11:28:12 +0200

    sort fix

--- src/Pages/CardList.js
+++ src/Pages/CardList.js
@@ -1,51 +1,118 @@
-import { faCheckSquare, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { addDoc, collection, getDocs, increment, query, updateDoc, where } from 'firebase/firestore'; 
+import { collection, deleteDoc, getDocs, limit, query, updateDoc, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../Styles/CardList.css';
-import { auth, db } from '../firebase';
+import { db } from '../firebase';
 
-function CardList() {
+function UserCardList({ userId }) {
   const [cards, setCards] = useState([]);
+  const [userCards, setUserCards] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [showModal, setShowModal] = useState(false);
-  const [modalImage, setModalImage] = useState('');
   const [searchTerm, setSearchTerm] = useState('');
-  const [selectedCards, setSelectedCards] = useState(new Map());
-  const [currentPage, setCurrentPage] = useState(1);
-  const [sortCriteria, setSortCriteria] = useState('');
-  const [sortOrder, setSortOrder] = useState('asc');
-  const cardsPerPage = 36;
+  const [sortCriteria, setSortCriteria] = useState(''); // Critério de ordenação
+  const [sortOrder, setSortOrder] = useState('asc'); // Ordem de ordenação
+  const [totalValue, setTotalValue] = useState(0); // Armazenar o valor total do deck
 
   useEffect(() => {
-    const fetchCards = async () => {
-      try {
-        setLoading(true);
-        const cachedCards = JSON.parse(localStorage.getItem('cards'));
-        const cacheTimestamp = localStorage.getItem('cards-timestamp');
-        const oneDay = 24 * 60 * 60 * 1000;
-      
-        if (cachedCards && cacheTimestamp && (Date.now() - cacheTimestamp) < oneDay) {
-          setCards(cachedCards);
-        } else {
-          const q = query(collection(db, "cards"));
-          const querySnapshot = await getDocs(q);
-          const fetchedCards = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-          localStorage.setItem('cards', JSON.stringify(fetchedCards));
-          localStorage.setItem('cards-timestamp', Date.now());
-          setCards(fetchedCards);
-        }
-      } catch (error) {
-        console.error("Error fetching cards from Firestore:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+    if (userId) {
+      fetchAllCards();
+      fetchCards(); 
+    }
+  }, [userId]);
 
-    fetchCards();
-  }, []);
+  useEffect(() => {
+    calculateTotalValue(); // Recalcular o valor total sempre que as cartas forem atualizadas
+  }, [userCards, cards]);
+
+  const fetchAllCards = async () => {
+    try {
+      setLoading(true);
+      const cachedCards = JSON.parse(localStorage.getItem('cards'));
+      const cacheTimestamp = localStorage.getItem('cards-timestamp');
+      const oneDay = 24 * 60 * 60 * 1000;
+
+      if (cachedCards && cacheTimestamp && (Date.now() - cacheTimestamp) < oneDay) {
+        setCards(cachedCards);
+      } else {
+        const q = query(collection(db, "cards"));
+        const querySnapshot = await getDocs(q);
+        const fetchedCards = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        localStorage.setItem('cards', JSON.stringify(fetchedCards));
+        localStorage.setItem('cards-timestamp', Date.now());
+        setCards(fetchedCards);
+      }
+    } catch (error) {
+      console.error("Error fetching cards from Firestore:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchCards = async () => {
+    try {
+      const q = query(collection(db, "userCards"), where("userId", "==", userId));
+      const querySnapshot = await getDocs(q);
+      const fetchedCards = querySnapshot.docs.map(doc => doc.data());
+      setUserCards(fetchedCards);
+    } catch (error) {
+      console.error("Erro ao buscar as cartas do usuário:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Função para calcular o valor total do deck
+  const calculateTotalValue = () => {
+    let total = 0;
+
+    userCards.forEach(userCard => {
+      const matchingCard = cards.find(card => card.id === userCard.cardId);
+      if (matchingCard && matchingCard.price !== "??") {
+        total += parseFloat(matchingCard.price) * userCard.count;
+      }
+    });
+
+    setTotalValue(total.toFixed(2));
+  };
+
+  const handleUpdateCards = async () => {
+    try {
+      toast.info("A atualizar cartas...");
+      for (const card of userCards) {
+        const q = query(
+          collection(db, 'userCards'),
+          where('userId', '==', userId),
+          where('cardId', '==', card.cardId),
+          limit(1)
+        );
+
+        const querySnapshot = await getDocs(q);
+        
+        if (!querySnapshot.empty) {
+          const doc = querySnapshot.docs[0];
+          const cardDocRef = doc.ref;
+
+          if (card.count === 0) {
+            await deleteDoc(cardDocRef); // Se a contagem for 0, remover a carta
+          } else {
+            await updateDoc(cardDocRef, {
+              count: card.count,
+            });
+          }
+        }
+      }
+
+      toast.success("Cartas atualizadas com sucesso!");
+
+      // Atualiza a lista de cartas após 3 segundos
+      setTimeout(() => {
+        fetchCards();
+      }, 3000);
+      
+    } catch (error) {
+      console.error("Erro ao atualizar cartas:", error);
+    }
+  };
 
   const handleSortChange = (e) => {
     setSortCriteria(e.target.value);
@@ -55,86 +122,45 @@ function CardList() {
     setSortOrder(e.target.value);
   };
 
-  const sortedCards = [...cards].sort((a, b) => {
-    let comparison = 0;
-    if (sortCriteria === 'id') {
-      comparison = a.id.localeCompare(b.id);
-    } else if (sortCriteria === 'name') {
-      comparison = a.name.localeCompare(b.name);
-    } else if (sortCriteria === 'quantity') {
-      comparison = (a.quantity || 0) - (b.quantity || 0);
-    }
-    return sortOrder === 'asc' ? comparison : -comparison;
-  });
-
-  const filteredCards = sortedCards.filter(card =>
-    card.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    card.id.includes(searchTerm)
-  );
-
-  const indexOfLastCard = currentPage * cardsPerPage;
-  const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-  const currentCards = filteredCards.slice(indexOfFirstCard, indexOfLastCard);
-
-  const toggleSelectCard = (id) => {
-    setSelectedCards(prev => {
-      const newSelection = new Map(prev);
-      if (newSelection.has(id)) {
-        newSelection.delete(id);
-      } else {
-        newSelection.set(id, 1);
+  const sortedCards = userCards
+    .map(userCard => {
+      const matchingCard = cards.find(card => card.id === userCard.cardId);
+      return {
+        ...userCard,
+        name: matchingCard ? matchingCard.name : 'Nome não disponível',
+      };
+    })
+    .filter(card =>
+      card.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      card.cardId.includes(searchTerm)
+    )
+    .sort((a, b) => {
+      let comparison = 0;
+      if (sortCriteria === 'id') {
+        comparison = a.cardId.localeCompare(b.cardId);
+      } else if (sortCriteria === 'name') {
+        comparison = a.name.localeCompare(b.name);
+      } else if (sortCriteria === 'quantity') {
+        comparison = a.count - b.count;
       }
-      return newSelection;
+      return sortOrder === 'asc' ? comparison : -comparison;
     });
-  };
-
-  const handleAddCards = async () => {
-    const userId = auth.currentUser.uid;
-    const selectedEntries = [...selectedCards.entries()];
-    try {
-      toast.info("A adicionar cartas...");
-      for (const [cardId, quantity] of selectedEntries) {
-        const q = query(
-          collection(db, "userCards"),
-          where("userId", "==", userId),
-          where("cardId", "==", cardId)
-        );
-        const querySnapshot = await getDocs(q);
-
-        if (!querySnapshot.empty) {
-          const docRef = querySnapshot.docs[0].ref;
-          await updateDoc(docRef, {
-            count: increment(quantity),
-          });
-        }
-        else {
-          await addDoc(collection(db, "userCards"), {
-            userId: userId,
-            cardId: cardId,
-            count: quantity,
-          });
-        }
-      }
-      toast.success("Cartas adicionadas com sucesso!");
-      setSelectedCards(new Map());
-    } catch (error) {
-      toast.error("Erro ao adicionar cartas.");
-    }
-  };
 
   return (
     <div>
       <ToastContainer />
 
-      <div className="controls">
+      {/* Exibe o valor total do deck */}
+      <h2 style={{ color: 'white', marginLeft: '55px', textAlign: 'left' }}>
+        Valor total do deck: {totalValue}€
+      </h2>
+
+      <div className="controls" style={{ marginLeft: '15px' }}>
         <input
           type="text"
           placeholder="Filtrar cartas..."
           value={searchTerm}
-          onChange={(e) => {
-            setSearchTerm(e.target.value);
-            setCurrentPage(1);
-          }}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className="filter-input"
         />
 
@@ -146,7 +172,7 @@ function CardList() {
             <option value="name">Nome</option>
             <option value="quantity">Quantidade</option>
           </select>
-          
+
           <label htmlFor="order">Ordem: </label>
           <select id="order" value={sortOrder} onChange={handleSortOrderChange}>
             <option value="asc">Crescente</option>
@@ -159,148 +185,86 @@ function CardList() {
         <p>A carregar cartas...</p>
       ) : (
         <div className="card-list">
-          {currentCards.length === 0 ? (
-            <p>Nenhuma carta encontrada.</p>
+          {sortedCards.length === 0 ? (
+            <p>Nenhuma carta adicionada.</p>
           ) : (
-            currentCards.map((card) => (
-              <div
-                className={card-item ${selectedCards.has(card.id) ? 'selected' : ''}}
-                key={card.id}
-                onClick={(e) => {
-                  if (e.target.tagName !== 'BUTTON' && e.target.tagName !== 'I') {
-                    toggleSelectCard(card.id);
-                  }
-                }}
-                style={{ position: 'relative' }}
-              >
-                <div className="select-box">
-                  {selectedCards.has(card.id) && (
-                    <FontAwesomeIcon icon={faCheckSquare} style={{ color: 'green' }} />
-                  )}
-                </div>
+            sortedCards.map((card) => (
+              <div className="card-item" key={card.cardId}>
                 <img
-                  src={https://static.dotgg.gg/onepiece/card/${card.id}.webp}
-                  alt={card.name}
+                  src={`https://static.dotgg.gg/onepiece/card/${card.cardId}.webp`}
+                  alt={`Carta ${card.cardId}`} 
                 />
-                <h2>{card.name}</h2>
+                <h2>{cards.find((c) => c.id === card.cardId)?.name || 'Nome não disponível'}</h2>
                 <p style={{ color: 'blue', fontWeight: 900 }}>
-                  {card.price !== "??" ? Number(card.price).toFixed(2) : card.price}€
+                  {cards.find((c) => c.id === card.cardId)?.price !== "??" 
+                    ? Number(cards.find((c) => c.id === card.cardId)?.price).toFixed(2) 
+                    : card.price}€
                 </p>
-                <p>ID: {card.id}</p>
-
-                <div
-                  className="magnify-icon"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setModalImage(https://static.dotgg.gg/onepiece/card/${card.id}.webp);
-                    setShowModal(true);
-                  }}
-                >
-                  <FontAwesomeIcon icon={faSearch} />
-                </div>
-
-                {selectedCards.has(card.id) && (
-                  <div style={{ display: 'flex', alignItems: 'center', gap: '10px', justifyContent: 'center' }}>
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setSelectedCards((prev) => {
-                          const newSelection = new Map(prev);
-                          const currentQuantity = newSelection.get(card.id) || 1;
-                          if (currentQuantity > 1) {
-                            newSelection.set(card.id, currentQuantity - 1);
+                <p>ID: {card.cardId}</p>
+                <div style={{ display: 'flex', alignItems: 'center', gap: '10px', justifyContent: 'center' }}>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setUserCards((prev) => 
+                        prev.map((c) => {
+                          if (c.cardId === card.cardId && c.count > 0) {
+                            return { ...c, count: c.count - 1 };
                           }
-                          return newSelection;
-                        });
-                      }}
-                      style={{
-                        padding: '5px 10px',
-                        fontSize: '16px',
-                        cursor: 'pointer',
-                        backgroundColor: '#dc3545',
-                        color: 'white',
-                        border: 'none',
-                        borderRadius: '4px',
-                      }}
-                    >
-                      -
-                    </button>
-                    <span style={{ fontSize: '16px', fontWeight: 'bold' }}>
-                      {selectedCards.get(card.id) || 1}
-                    </span>
-                    <button
-                      onClick={(e) => {
-                        e.stopPropagation();
-                        setSelectedCards((prev) => {
-                          const newSelection = new Map(prev);
-                          const currentQuantity = newSelection.get(card.id) || 1;
-                          newSelection.set(card.id, currentQuantity + 1);
-                          return newSelection;
-                        });
-                      }}
-                      style={{
-                        padding: '5px 10px',
-                        fontSize: '16px',
-                        cursor: 'pointer',
-                        backgroundColor: '#28a745',
-                        color: 'white',
-                        border: 'none',
-                        borderRadius: '4px',
-                      }}
-                    >
-                      +
-                    </button>
-                  </div>
-                )}
-
-                {showModal && (
-                  <div className="modal" onClick={() => setShowModal(false)}>
-                    <div className="modal-content">
-                      <span className="close" onClick={() => setShowModal(false)}>&times;</span>
-                      <img src={modalImage} alt="Enlarged Card" className="enlarged-card-image" />
-                      <button 
-                        onClick={() => setShowModal(false)} 
-                        style={{
-                          position: 'absolute',
-                          top: '10px',
-                          right: '10px',
-                          backgroundColor: 'red',
-                          color: 'white',
-                          border: 'none',
-                          padding: '5px 10px',
-                          borderRadius: '5px',
-                          cursor: 'pointer'
-                        }}>
-                        X
-                      </button>
-                    </div>
-                  </div>
-                )}
+                          return c;
+                        })
+                      );
+                    }}
+                    style={{
+                      padding: '5px 10px',
+                      fontSize: '16px',
+                      cursor: 'pointer',
+                      backgroundColor: '#dc3545',
+                      color: 'white',
+                      border: 'none',
+                      borderRadius: '4px',
+                    }}
+                  >
+                    -
+                  </button>
+                  <span style={{ fontSize: '16px', fontWeight: 'bold' }}>
+                    {card.count}
+                  </span>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setUserCards((prev) => 
+                        prev.map((c) => {
+                          if (c.cardId === card.cardId) {
+                            return { ...c, count: c.count + 1 };
+                          }
+                          return c;
+                        })
+                      );
+                    }}
+                    style={{
+                      padding: '5px 10px',
+                      fontSize: '16px',
+                      cursor: 'pointer',
+                      backgroundColor: '#28a745',
+                      color: 'white',
+                      border: 'none',
+                      borderRadius: '4px',
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
               </div>
-
             ))
           )}
         </div>
       )}
 
-      {selectedCards.size > 0 && (
-        <button onClick={handleAddCards} className="add-cards-button">Adicionar Cartas</button>
-      )}
-
-      <div className="pagination">
-        {Array.from({ length: Math.ceil(filteredCards.length / cardsPerPage) }, (_, index) => (
-          <button
-            key={index + 1}
-            onClick={() => setCurrentPage(index + 1)}
-            className={currentPage === index + 1 ? 'active' : ''}
-            disabled={currentPage === index + 1}
-          >
-            {index + 1}
-          </button>
-        ))}
-      </div>
+      <button onClick={handleUpdateCards} className="remove-cards-button">
+        Atualizar Cartas
+      </button>
     </div>
   );
 }
 
-export default CardList;
+export default UserCardList;
